1246311e76a42a0aaf428ca6e2080c70c7290da3
adhammohamed1 <a.mohamed191100@gmail.com>
2022-10-30 15:29:02 +0200

    (1) Fixed DFS final counter (2) Added isSolvable() function (3) Code cleanup

--- main.py
+++ main.py
@@ -4,27 +4,28 @@ import heapq
 dx = [-1, 1, 0, 0]
 dy = [0, 0, 1, -1]
 
-dfs_counter=0
-bfs_counter=0
-euclid_counter=0
-manhattan_counter=0
+dfs_counter = 0
+bfs_counter = 0
+euclid_counter = 0
+manhattan_counter = 0
 
-dfs_path=[]
-bfs_path=[]
-euclid_path=[]
-manhattan_path=[]
+dfs_path = []
+bfs_path = []
+euclid_path = []
+manhattan_path = []
 
-dfs_cost=0
-bfs_cost=0
-euclid_cost=0
-manhattan_cost=0
+dfs_cost = 0
+bfs_cost = 0
+euclid_cost = 0
+manhattan_cost = 0
 
 
 def getStringRepresentation(x):
-    if(int(math.log10(x))+1 == 9):
+    if int(math.log10(x)) + 1 == 9:
         return str(x)
-    else :
-        return "0"+str(x)    
+    else:
+        return "0" + str(x)
+
 
 def getChildren(state):
     children = []
@@ -40,7 +41,9 @@ def getChildren(state):
             listTemp[idx], listTemp[nwIdx] = listTemp[nwIdx], listTemp[idx]
             children.append(''.join(listTemp))
     return children
-def getPath(parentMap,inputState):
+
+
+def getPath(parentMap, inputState):
     path = []
     temp = 12345678
     while temp != inputState:
@@ -49,80 +52,102 @@ def getPath(parentMap,inputState):
     path.append(inputState)
     path.reverse()
     return path
+
+
 def printPath(path):
     for i in path:
         print(getStringRepresentation(i))
+
+
 def goalTest(state):
     if state == 12345678:
         return True
     return False
+
+
+def isSolvable(digit):
+    count = 0
+    for i in range(0, 9):
+        for j in range(i, 9):
+            if digit[i] > digit[j] and digit[i] != 9:
+                count += 1
+    return count % 2 == 0
+
+
 def BFS(inputState):
+    # if not isSolvable(inputState):
+    #     print("Not Solvable")
+    #     return 0
     q = []
     explored = {}
     parent = {}
-    integer_state=int(inputState)
+    integer_state = int(inputState)
     q.append(integer_state)  # here you place the input
-    cnt=0
+    cnt = 0
     global bfs_counter
     global bfs_path
     global bfs_cost
     while q:
-        cnt+=1
+        cnt += 1
         state = q.pop(0)
         explored[state] = 1
         if goalTest(state):
-            path = getPath(parent,int(inputState))
-            #printPath(path)
-            bfs_counter=cnt
-            bfs_path=path
-            bfs_cost=len(path)-1
+            path = getPath(parent, int(inputState))
+            # printPath(path)
+            bfs_counter = cnt
+            bfs_path = path
+            bfs_cost = len(path) - 1
             return 1
 
         children = getChildren(getStringRepresentation(state))
         for child in children:
-            child_int=int(child)
-            if not child_int in explored:
+            child_int = int(child)
+            if child_int not in explored:
                 q.append(child_int)
                 parent[child_int] = state
-                explored[child_int]=1
-    bfs_path=[]    
-    bfs_cost=0                
-    bfs_counter=cnt
+                explored[child_int] = 1
+    bfs_path = []
+    bfs_cost = 0
+    bfs_counter = cnt
     return 0
 
 
 def DFS(inputState):
+    # if not isSolvable(inputState):
+    #     print("Not Solvable")
+    #     return 0
     stack = []
     explored = {}
     parent = {}
-    integer_state=int(inputState)
+    integer_state = int(inputState)
+    explored[integer_state] = 1
     stack.append(integer_state)
-    cnt=0
+    cnt = 0
     global dfs_counter
     global dfs_path
     global dfs_cost
     while stack:
-        cnt+=1
+        cnt += 1
         state = stack[-1]
         stack.pop()
         if goalTest(state):
-            path = getPath(parent,int(inputState))
-            #printPath(path)
-            dfs_counter=cnt
-            dfs_path=path
-            dfs_cost=len(path)-1
+            path = getPath(parent, int(inputState))
+            # printPath(path)
+            dfs_counter = cnt
+            dfs_path = path
+            dfs_cost = len(path) - 1
             return 1
 
         children = getChildren(getStringRepresentation(state))
         for child in children:
-            child_int=int(child)
-            if not child_int in explored:
+            child_int = int(child)
+            if child_int not in explored:
                 stack.append(child_int)
-                parent[child_int]=state
-                explored[child_int]=1
-    dfs_path=[]    
-    dfs_cost=0            
-    dfs_counter=cnt            
+                parent[child_int] = state
+                explored[child_int] = 1
+    dfs_path = []
+    dfs_cost = 0
+    dfs_counter = cnt
     return 0
 
 
@@ -133,31 +158,34 @@ def checkValid(i, j):
 
 
 def getManhattanDistance(state):
-    sum = 0
+    tot = 0
     for i in range(1, 9):
         goalX = int(i / 3)
         goalY = i % 3
         idx = state.index(str(i))
         itemX = int(idx / 3)
         itemY = idx % 3
-        sum += (abs(goalX - itemX) + abs(goalY - itemY))
-    return sum
+        tot += (abs(goalX - itemX) + abs(goalY - itemY))
+    return tot
 
 
 def getEuclideanDistance(state):
-    sum = 0
+    tot = 0
     for i in range(1, 9):
         goalX = int(i / 3)
         goalY = i % 3
         idx = state.index(str(i))
         itemX = int(idx / 3)
         itemY = idx % 3
-        sum += math.sqrt(pow((goalX - itemX), 2) + pow((goalY - itemY), 2))
-    return sum
+        tot += math.sqrt(pow((goalX - itemX), 2) + pow((goalY - itemY), 2))
+    return tot
+
 
 def AStarSearch_manhattan(inputState):
-    
-    integer_state=int(inputState)
+    # if not isSolvable(inputState):
+    #     print("Not Solvable")
+    #     return 0
+    integer_state = int(inputState)
     heap = []
     explored = {}
     parent = {}
@@ -172,38 +200,42 @@ def AStarSearch_manhattan(inputState):
     while heap:
         node = heapq.heappop(heap)
         state = node[1]
-        string_state=getStringRepresentation(state)
-        parent_cost = node[0]-getManhattanDistance(string_state)
+        string_state = getStringRepresentation(state)
+        parent_cost = node[0] - getManhattanDistance(string_state)
         explored[state] = 1
         if goalTest(state):
-            path = getPath(parent,int(inputState))
-            #printPath(path)
-            manhattan_path=path
-            manhattan_counter=(len(explored))
-            manhattan_cost=len(path)-1
+            path = getPath(parent, int(inputState))
+            # printPath(path)
+            manhattan_path = path
+            manhattan_counter = (len(explored))
+            manhattan_cost = len(path) - 1
             return 1
-        
+
         children = getChildren(string_state)
         for child in children:
             new_cost = getManhattanDistance(child)
-            child_int =int(child)
-            if not child_int in explored and not child in heap_map:
-                heapq.heappush(heap, (parent_cost + new_cost +1, child_int))
+            child_int = int(child)
+            if child_int not in explored and child not in heap_map:
+                heapq.heappush(heap, (parent_cost + new_cost + 1, child_int))
                 heap_map[child_int] = 1
-                cost_map[child_int] = parent_cost + new_cost +1
+                cost_map[child_int] = parent_cost + new_cost + 1
                 parent[child_int] = state
             elif child_int in heap_map:
-                if (new_cost + parent_cost +1) < cost_map[child_int]:
+                if (new_cost + parent_cost + 1) < cost_map[child_int]:
                     parent[child_int] = state
-                    cost_map[child_int] = new_cost + parent_cost +1
+                    cost_map[child_int] = new_cost + parent_cost + 1
                     heapq.heappush(heap, (parent_cost + 1 + new_cost, child_int))
-    manhattan_cost=0                    
-    manhattan_path=[]                    
-    manhattan_counter=(len(explored))
+    manhattan_cost = 0
+    manhattan_path = []
+    manhattan_counter = (len(explored))
     return 0
+
+
 def AStarSearch_euclid(inputState):
-    
-    integer_state=int(inputState)
+    # if not isSolvable(inputState):
+    #     print("Not Solvable")
+    #     return 0
+    integer_state = int(inputState)
     heap = []
     explored = {}
     parent = {}
@@ -218,36 +250,35 @@ def AStarSearch_euclid(inputState):
     while heap:
         node = heapq.heappop(heap)
         state = node[1]
-        string_state=getStringRepresentation(state)
-        parent_cost = node[0]-getEuclideanDistance(string_state)
+        string_state = getStringRepresentation(state)
+        parent_cost = node[0] - getEuclideanDistance(string_state)
         explored[state] = 1
         if goalTest(state):
-            path = getPath(parent,int(inputState))
-            #printPath(path)
-            euclid_path=path
-            euclid_counter=(len(explored))
-            euclid_cost=len(path)-1
+            path = getPath(parent, int(inputState))
+            # printPath(path)
+            euclid_path = path
+            euclid_counter = (len(explored))
+            euclid_cost = len(path) - 1
             return 1
-        
+
         children = getChildren(string_state)
         for child in children:
             new_cost = getEuclideanDistance(child)
-            child_int =int(child)
-            if not child_int in explored and not child in heap_map:
-                heapq.heappush(heap, (parent_cost + new_cost +1, child_int))
+            child_int = int(child)
+            if child_int not in explored and not child in heap_map:
+                heapq.heappush(heap, (parent_cost + new_cost + 1, child_int))
                 heap_map[child_int] = 1
-                cost_map[child_int] = parent_cost + new_cost +1
+                cost_map[child_int] = parent_cost + new_cost + 1
                 parent[child_int] = state
             elif child_int in heap_map:
-                if (new_cost + parent_cost +1) < cost_map[child_int]:
+                if (new_cost + parent_cost + 1) < cost_map[child_int]:
                     parent[child_int] = state
-                    cost_map[child_int] = new_cost + parent_cost +1
+                    cost_map[child_int] = new_cost + parent_cost + 1
                     heapq.heappush(heap, (parent_cost + 1 + new_cost, child_int))
-    euclid_cost=0
-    euclid_path=[]                
-    euclid_counter=(len(explored))
+    euclid_cost = 0
+    euclid_path = []
+    euclid_counter = (len(explored))
     return 0
-
 
 
 # BFS("583704126")
@@ -257,7 +288,6 @@ def AStarSearch_euclid(inputState):
 # AStarSearch_euclid("583704126")
 # print("---------------")
 # print(AStarSearch_manhattan("583704126"))
-
 
 
 DFS("123045678")
@@ -277,19 +307,21 @@ print(euclid_path)
 print("---------------")
 print(manhattan_path)
 print("---------------")
-print(str(dfs_counter)+" "+str(bfs_counter)+" "+str(euclid_counter)+" "+str(manhattan_counter))
+print(str(dfs_counter) + " " + str(bfs_counter) + " " + str(euclid_counter) + " " + str(manhattan_counter))
 print("---------------")
-print(str(dfs_cost)+" "+str(bfs_cost)+" "+str(euclid_cost)+" "+str(manhattan_cost))
+print(str(dfs_cost) + " " + str(bfs_cost) + " " + str(euclid_cost) + " " + str(manhattan_cost))
 
 #  7,0,2,8,5,3,6,4,1 unsolvable state
 print("---------------")
 print(DFS("702853641"))
 print("---------------")
+print(BFS("702853641"))
+print("---------------")
 print(AStarSearch_euclid("702853641"))
 print("---------------")
 print(AStarSearch_manhattan("702853641"))
 print("---------------")
-print(BFS("702853641"))
+
 print(dfs_path)
 print("---------------")
 print(bfs_path)
@@ -298,11 +330,9 @@ print(euclid_path)
 print("---------------")
 print(manhattan_path)
 print("---------------")
-print(str(dfs_counter)+" "+str(bfs_counter)+" "+str(euclid_counter)+" "+str(manhattan_counter))
+print(str(dfs_counter) + " " + str(bfs_counter) + " " + str(euclid_counter) + " " + str(manhattan_counter))
 print("---------------")
-print(str(dfs_cost)+" "+str(bfs_cost)+" "+str(euclid_cost)+" "+str(manhattan_cost))
-
-
+print(str(dfs_cost) + " " + str(bfs_cost) + " " + str(euclid_cost) + " " + str(manhattan_cost))
 
 # print("---------------")
 # n=213
@@ -325,7 +355,7 @@ print(str(dfs_cost)+" "+str(bfs_cost)+" "+str(euclid_cost)+" "+str(manhattan_cos
 # print("fady")
 
 ##################### sibha hena matshelhash lhad ma2olak ##################################
-#def UCS(inputState):
+# def UCS(inputState):
 #     heap = []
 #     explored = {}
 #     parent = {}
@@ -357,4 +387,3 @@ print(str(dfs_cost)+" "+str(bfs_cost)+" "+str(euclid_cost)+" "+str(manhattan_cos
 #                     cost_map[child] = 1 + parent_cost
 #                     heapq.heappush(heap, (parent_cost + 1, child))
 #     return 0
-
